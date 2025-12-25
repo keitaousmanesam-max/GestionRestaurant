f66b2f9f5948e1a22541dd86d343138a7dbb68f0
samkeita <samkeita@gmail.com>
2025-12-25 13:14:04 +0100

    Creation des Packages necessary

---
+++ src/database/TestDBConnection.java
@@ -0,0 +1,31 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class TestDBConnection {
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/gestion_restaurant?useSSL=false&serverTimezone=UTC";
+        String user = "root";
+        String password = "Sam219592";
+
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connexion réussie ✅");
+
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT DATABASE()");
+
+            if (rs.next()) {
+                System.out.println("Base utilisée : " + rs.getString(1));
+            }
+
+            conn.close();
+        } catch (Exception e) {
+            System.out.println("Erreur : " + e.getMessage());
+        }
+    }
+}
